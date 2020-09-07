e7458db2bde0c929564bda3caf5bf41fce335e5c
Koyamie <koyamie1@gmail.com>
2020-09-07 20:58:50 +0200

    Update RequestHandler.js

--- src/rest/RequestHandler.js
+++ src/rest/RequestHandler.js
@@ -3,7 +3,7 @@
 const DiscordAPIError = require('./DiscordAPIError');
 const HTTPError = require('./HTTPError');
 const {
-  Events: { RATE_LIMIT },
+  Events: { RATE_LIMIT, INVALID_REQUEST_WARNING },
   browser,
 } = require('../util/Constants');
 const Util = require('../util/Util');
@@ -22,6 +22,15 @@ function calculateReset(reset, serverDate) {
   return new Date(Number(reset) * 1000).getTime() - getAPIOffset(serverDate);
 }
 
+/* Invalid request limiting is done on a per-IP basis, not a per-token basis.
+ * The best we can do is track invalid counts process-wide (on the theory that
+ * users could have multiple bots run from one process) rather than per-bot.
+ * Therefore, store these at file scope here rather than in the client's
+ * RESTManager object.
+ */
+let invalidCount = 0;
+let invalidCountResetTime = null;
+
 class RequestHandler {
   constructor(manager) {
     this.manager = manager;
@@ -30,7 +39,6 @@ class RequestHandler {
     this.reset = -1;
     this.remaining = -1;
     this.limit = -1;
-    this.retryAfter = -1;
   }
 
   push(request) {
@@ -47,12 +55,33 @@ class RequestHandler {
     return this.execute(this.queue.shift());
   }
 
+  get globalLimited() {
+    return this.manager.globalRemaining <= 0 && Date.now() < this.manager.globalReset;
+  }
+
+  get localLimited() {
+    return this.remaining <= 0 && Date.now() < this.reset;
+  }
+
   get limited() {
-    return Boolean(this.manager.globalTimeout) || (this.remaining <= 0 && Date.now() < this.reset);
+    return this.globalLimited || this.localLimited;
   }
 
   get _inactive() {
     return this.queue.length === 0 && !this.limited && this.busy !== true;
+  }
+
+  globalDelayFor(ms) {
+    return new Promise(resolve => {
+      this.manager.client.setTimeout(
+        manager => {
+          manager.globalDelay = null;
+          resolve();
+        },
+        ms,
+        this.manager,
+      );
+    });
   }
 
   async execute(item) {
@@ -65,9 +94,31 @@ class RequestHandler {
     this.busy = true;
     const { reject, request, resolve } = item;
 
-    // After calculations and requests have been done, pre-emptively stop further requests
-    if (this.limited) {
-      const timeout = this.reset + this.manager.client.options.restTimeOffset - Date.now();
+    /*
+     * After calculations have been done, pre-emptively stop further requests
+     * Potentially loop until this task can run if e.g. the global rate limit is hit twice
+     */
+    while (this.limited) {
+      let global, limit, timeout, delayPromise;
+
+      if (this.globalLimited) {
+        // Set the variables based on the global rate limit
+        global = true;
+        limit = this.manager.globalLimit;
+        timeout = this.manager.globalReset + this.manager.client.options.restTimeOffset - Date.now();
+        // If this is the first task to reach the global timeout, set the global delay
+        if (!this.manager.globalDelay) {
+          // The global delay function should clear the global delay state when it is resolved
+          this.manager.globalDelay = this.globalDelayFor(timeout);
+        }
+        delayPromise = this.manager.globalDelay;
+      } else {
+        // Set the variables based on the route-specific rate limit
+        global = false;
+        limit = this.limit;
+        timeout = this.reset + this.manager.client.options.restTimeOffset - Date.now();
+        delayPromise = Util.delayFor(timeout);
+      }
 
       if (this.manager.client.listenerCount(RATE_LIMIT)) {
         /**
@@ -79,30 +130,35 @@ class RequestHandler {
          * @param {string} rateLimitInfo.method HTTP method used for request that triggered this event
          * @param {string} rateLimitInfo.path Path used for request that triggered this event
          * @param {string} rateLimitInfo.route Route used for request that triggered this event
+         * @param {boolean} rateLimitInfo.global Whether the rate limit that was reached was the global limit
          */
         this.manager.client.emit(RATE_LIMIT, {
           timeout,
-          limit: this.limit,
+          limit: limit,
           method: request.method,
           path: request.path,
           route: request.route,
+          global: global,
         });
       }
 
-      if (this.manager.globalTimeout) {
-        await this.manager.globalTimeout;
-      } else {
-        // Wait for the timeout to expire in order to avoid an actual 429
-        await Util.delayFor(timeout);
-      }
+      // Wait for the timeout to expire in order to avoid an actual 429
+      await delayPromise; // eslint-disable-line no-await-in-loop
     }
+
+    // As the request goes out, update the global usage information
+    if (!this.manager.globalReset || this.manager.globalReset < Date.now()) {
+      this.manager.globalReset = Date.now() + 1000;
+      this.manager.globalRemaining = this.manager.globalLimit;
+    }
+    this.manager.globalRemaining--;
 
     // Perform the request
     let res;
     try {
       res = await request.make();
     } catch (error) {
-      // NodeFetch error expected for all "operational" errors, such as 500 status code
+      // NodeFetch error expected for all "operational" errors, which does **not** include 3xx-5xx status codes
       this.busy = false;
       return reject(new HTTPError(error.message, error.constructor.name, error.status, request.method, request.path));
     }
@@ -112,37 +168,63 @@ class RequestHandler {
       const limit = res.headers.get('x-ratelimit-limit');
       const remaining = res.headers.get('x-ratelimit-remaining');
       const reset = res.headers.get('x-ratelimit-reset');
-      const retryAfter = res.headers.get('retry-after');
-
       this.limit = limit ? Number(limit) : Infinity;
       this.remaining = remaining ? Number(remaining) : 1;
       this.reset = reset ? calculateReset(reset, serverDate) : Date.now();
-      this.retryAfter = retryAfter ? Number(retryAfter) : -1;
 
       // https://github.com/discordapp/discord-api-docs/issues/182
       if (item.request.route.includes('reactions')) {
         this.reset = new Date(serverDate).getTime() - getAPIOffset(serverDate) + 250;
       }
 
-      // Handle global ratelimit
-      if (res.headers.get('x-ratelimit-global')) {
-        // Set the manager's global timeout as the promise for other requests to "wait"
-        this.manager.globalTimeout = Util.delayFor(this.retryAfter);
-
-        // Wait for the global timeout to resolve before continuing
-        await this.manager.globalTimeout;
-
-        // Clean up global timeout
-        this.manager.globalTimeout = null;
+      // Handle retryAfter, which means we have actually hit a rate limit
+      let retryAfter = res.headers.get('retry-after');
+      retryAfter = retryAfter ? Number(retryAfter) : -1;
+      if (retryAfter > 0) {
+        // If the global ratelimit header is set, that means we hit the global rate limit
+        if (res.headers.get('x-ratelimit-global')) {
+          this.manager.globalRemaining = 0;
+          this.manager.globalReset = Date.now() + retryAfter;
+        } else if (!this.localLimited) {
+          /*
+           * This is a sublimit (e.g. 2 channel name changes/10 minutes) since the headers don't indicate a
+           * route-wide rate limit. Don't update remaining or reset to avoid rate limit the whole endpoint,
+           * just set a reset time on the request itself to avodi retrying too soon.
+           */
+          res.sublimit = retryAfter;
+        }
       }
     }
 
     // Finished handling headers, safe to unlock manager
     this.busy = false;
-    
-    if (this.manager.client.dogstats) this.manager.client.dogstats.increment("koya.requesthandler", { status: res.status });
-    
-    this.manager.client.logger.warn(`[REQUEST HANDLER] ${item.request.method.toUpperCase()} ${item.request.route}`, JSON.stringify(item.request.options.data));
+
+    // Count the invalid requests
+    if (res.status === 401 || res.status === 403 || res.status === 429) {
+      if (!invalidCountResetTime || invalidCountResetTime < Date.now()) {
+        invalidCountResetTime = Date.now() + 1000 * 60 * 10;
+        invalidCount = 0;
+      }
+      invalidCount++;
+
+      const emitInvalid =
+        this.manager.client.listenerCount(INVALID_REQUEST_WARNING) &&
+        this.manager.client.options.invalidRequestWarningInterval > 0 &&
+        invalidCount % this.manager.client.options.invalidRequestWarningInterval === 0;
+      if (emitInvalid) {
+        /**
+         * Emitted periodically when the process sends invalid messages to let users avoid the
+         * 10k invalid messages in 10 minutes threshold that causes a ban
+         * @event Client#invalidRequestWarning
+         * @param {number} invalidRequestWarningInfo.count Number of invalid requests that have been made in the window
+         * @param {number} invalidRequestWarningInfo.remainingTime Time in ms remaining before the count resets
+         */
+        this.manager.client.emit(INVALID_REQUEST_WARNING, {
+          count: invalidCount,
+          remainingTime: invalidCountResetTime - Date.now(),
+        });
+      }
+    }
 
     if (res.ok) {
       const success = await parseResponse(res);
@@ -151,10 +233,14 @@ class RequestHandler {
       return this.run();
     } else if (res.status === 429) {
       // A ratelimit was hit - this should never happen
+      this.manager.client.emit('debug', `429 hit on route ${item.request.route}${res.sublimit ? ' for sublimit' : ''}`);
+      // If caused by a sublimit, wait it out here so other requests on the route can be handled
+      if (res.sublimit) {
+        await Util.delayFor(res.sublimit);
+        delete res.sublimit;
+      }
+      // Don't put the item back on the queue until after waiting out any sublimit it may have
       this.queue.unshift(item);
-      this.manager.client.logger.warn(`[RATELIMIT] ${item.request.method.toUpperCase()} ${item.request.route}`, JSON.stringify(item.request.options.data));
-      this.manager.client.emit('debug', `429 hit on route ${item.request.route}`);
-      await Util.delayFor(this.retryAfter);
       return this.run();
     } else if (res.status >= 500 && res.status < 600) {
       // Retry the specified number of times for possible serverside issues
